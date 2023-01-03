7605e569e4d23b9556812eae5916a70bc381dd63
xiao <xiao@itcast.cn>
2023-01-03 18:35:59 +0800

    second commit

--- src/main/java/com/xiao/test.java
+++ src/main/java/com/xiao/test.java
@@ -3,5 +3,6 @@ package com.xiao;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git! 2 ");
     }
 }
