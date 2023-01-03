d11804fb05c8fb915a2ea8b40f9eea3bc4967e50
xiao <xiao@itcast.cn>
2023-01-03 18:51:45 +0800

    第三次提交 master commit

--- src/main/java/com/xiao/test.java
+++ src/main/java/com/xiao/test.java
@@ -5,5 +5,6 @@ public class test {
         System.out.println("Hello Git!");
         System.out.println("Hello Git! 2 ");
         System.out.println("Hello Git! 3 ");
+        System.out.println("master test");
     }
 }
