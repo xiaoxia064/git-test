7270597dd369b03df37a45738a9fbdc2afef614a
xiao <xiao@itcast.cn>
2023-01-04 15:05:34 +0800

    push-test commit

--- src/main/java/com/xiao/test.java
+++ src/main/java/com/xiao/test.java
@@ -7,5 +7,6 @@ public class test {
         System.out.println("Hello Git! 3 ");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
